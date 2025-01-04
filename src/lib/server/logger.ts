386682f34df00c4ce40a1935fc74631f16a5ffbb
Andreas Olsson <noname.olsson@gmail.com>
2025-01-04 09:48:17 +0100

    add betterstack

--- src/lib/server/logger.ts
+++ src/lib/server/logger.ts
@@ -1,16 +1,26 @@
 import pino from 'pino';
+const token = '7Kaz7yzi9f6aPkMhTomCtWVv';
+
+// const transport = pino.transport({
+// 	target: '@logtail/pino',
+// 	options: { sourceToken: token }
+// });
 
 const logger = pino({
 	level: process.env.NODE_ENV === 'production' ? 'info' : 'debug',
-	transport:
-		process.env.NODE_ENV !== 'production'
-			? {
-					target: 'pino-pretty',
-					options: {
-						colorize: true
-					}
-				}
-			: undefined
+	transport: {
+		target: '@logtail/pino',
+		options: { sourceToken: token }
+	}
+	// transport:
+	// 	process.env.NODE_ENV !== 'production'
+	// 		? {
+	// 				target: 'pino-pretty',
+	// 				options: {
+	// 					colorize: true
+	// 				}
+	// 			}
+	// 		: undefined
 });
 
 export { logger };
